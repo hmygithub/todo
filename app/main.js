759a2ddd0721485669dedf382307e63ed431b495
hmygithub <2713237776@qq.com>
2017-09-29 17:29:08 +0800

    修改入口组件

--- app/main.js
+++ app/main.js
@@ -3,12 +3,18 @@
  */
 import React from 'react'
 import {render} from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 //引入组件
 import App from './App.js'
+import todoApp from '../Reducers/reducers'
 
+let store = createStore(todoApp)
 //注意下面引用的名称跟上面（import 后的名称）要一致（首字母大写）
 //使用、挂在组件，两个参数
 render(
-    <App></App>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app-container')
 )
